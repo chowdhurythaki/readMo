60f27ca6b569f051f456458d4cb1dce54144f0a3
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-08-27 16:35:14 -0700

    Filter api response for results with page count

--- server/helpers.js
+++ server/helpers.js
@@ -1,8 +1,6 @@
-// import express from 'express';
-// import request from 'request-promise';
-
 const express = require('express');
 const request = require('request-promise');
+
 
 const app = express();
 const gbSearch = {
@@ -17,8 +15,33 @@ const verifyUser = () => {
 const searchbyTitle = (title, callback) => {
   const query = `${gbSearch.url}${title}&key=${gbSearch.key}`;
   request(query)
-    .then(results => {
-      callback(null, results);
+    .then(response => {
+      const parsedResponse = JSON.parse(response);
+      // filter for relevant fields
+      const filtered = parsedResponse.items.reduce((accum, book) =>
+        {
+          if (book.volumeInfo.pageCount) {
+            const {
+              title,
+              authors,
+              description,
+              pageCount,
+              publishedDate,
+            } = book.volumeInfo;
+
+            accum.push({
+              title,
+              authors,
+              description,
+              pageCount,
+              publishedDate,
+              thumbnail: book.volumeInfo.imageLinks.thumbnail,
+            });
+          }
+          return accum;
+        }, []);
+
+      callback(null, filtered);
     })
     .catch(err => {
       throw err;
