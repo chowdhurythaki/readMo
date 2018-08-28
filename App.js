99ff44bcb76905da20e1f77a1653f19bd3300c4a
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-08-28 04:16:07 -0700

    Update dependencies list and entry point

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import App from './client/src/components/App';
+
+import { createStackNavigator } from 'react-navigation';
+import List from './client/src/components/List';
+import Search from './client/src/components/Search';
+import DatePicker from './client/src/components/DatePicker';
+import Breakdown from './client/src/components/Breakdown';
 
 export default class Entry extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <App />
-      </View>
+      <AppStackNavigator />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const AppStackNavigator = createStackNavigator({
+  Search: Search,
+  List: List,
+  DatePicker: DatePicker,
+  Breakdown: Breakdown,
+})
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from '../client/components/App';
+import App from '../client/src/components/App';
 
 import renderer from 'react-test-renderer';
 
